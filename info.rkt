c0dad5b54a06b04286827a1010d80180a31b712f
Jeffrey Massung <massung@gmail.com>
2020-02-02 12:27:37 -0600

    Updated info.

--- info.rkt
+++ info.rkt
@@ -1,6 +1,8 @@
 #lang info
 
 (define collection "csfml")
+(define pkg-authors '("massung@gmail.com"))
 (define version "1.0")
+(define pkg-desc "FFI bindings for the CSFML library")
 (define deps '("base"))
 (define compile-omit-paths '("examples"))
